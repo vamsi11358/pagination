b23e710c4f938bd79753b2d990524780d72d8660
vamsi11358 <vamsi11358@gmail.com>
2024-07-16 20:18:59 +0530

    error1

--- src/component/component.jsx
+++ src/component/component.jsx
@@ -11,7 +11,7 @@ const MyComponent = () => {
       try {
         const response = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+         console.log('failed to fetch data');
         }
         const result = await response.json();
         setData(result);
